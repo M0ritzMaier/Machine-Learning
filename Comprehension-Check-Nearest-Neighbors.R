b4fc3dc10cc720f8f1e513e3a9570494fb2e8364
M0ritzMaier <moritzmaier@moritzmsmacbook.home>
2019-06-05 19:34:26 +0200

    Added Q2

--- Comprehension-Check-Nearest-Neighbors.R
+++ Comprehension-Check-Nearest-Neighbors.R
@@ -25,5 +25,27 @@ max(F_1)
 ks[F_1 == max(F_1)]
 
 
+#---    Q2      ----
 library(dslabs)
+library(caret)
 data("tissue_gene_expression")
+ks <- c(1,3,5,6,7,9,11)
+set.seed(1, sample.kind = "Rounding")
+test_index <- createDataPartition(tissue_gene_expression$y, times = 1, p = 0.5, list = F)
+
+train_set <- tissue_gene_expression$x[test_index,]
+test_set <- tissue_gene_expression$x[-test_index,]
+y_train <- tissue_gene_expression$y[test_index]
+y_test <- tissue_gene_expression$y[-test_index]
+
+
+sapply(ks, function(k) {
+       
+        fit <- knn3(train_set, y_train,  k = k)
+        
+        y_hat <- predict(fit, newdata = test_set, type = "class")
+        
+        oa <- confusionMatrix(data = y_hat, reference = y_test)$overall[1]
+        names(oa) <- k
+        oa
+})
